15a0d77930cb28b1730d4f2c1eed6aa0da88a472
Bryton Scott Wilson <brytonsw@hulk.cs.byu.edu>
2023-02-21 20:50:03 -0700

    landing page design

---
+++ deployFiles.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+while getopts k:h:s: flag
+do
+    case "${flag}" in
+        k) key=${OPTARG};;
+        h) hostname=${OPTARG};;
+        s) service=${OPTARG};;
+    esac
+done
+
+if [[ -z "$key" ]]; then
+    printf "\nMissing required parameter: key\n"
+    exit 1
+fi
+if [[ -z "$hostname" ]]; then
+    printf "\nMissing required parameter: hostname\n"
+    exit 1
+fi
+if [[ -z "$key" ]]; then
+    printf "\nMissing required parameter: service\n"
+    exit 1
+fi
+
+if [[ -z "$key" || -z "$hostname" || -z "$service" ]]; then
+    printf " syntax: deployFiles.sh -k <pem key file> -h <hostname> -s <service>\n\n"
+    exit 1
+fi
+
+printf "\n----> Deploying files for $service to $hostname with $key\n"
+
+# Step 1
+printf "\n----> Clear out the previous distribution on the target.\n"
+ssh -i "$key" ubuntu@$hostname << ENDSSH
+rm -rf services/${service}/public
+mkdir -p services/${service}/public
+ENDSSH
+
+# Step 2
+printf "\n----> Copy the distribution package to the target.\n"
+scp -r -i "$key" * ubuntu@$hostname:services/$service/public
