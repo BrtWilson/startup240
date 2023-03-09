e9e61adad23682154d5f5396ccb2199a28beb134
Bryton Scott Wilson <brytonsw@delaware.cs.byu.edu>
2023-03-09 16:25:26 -0700

    Profile page data loadable; Login determines loaded data

--- login.js
+++ login.js
@@ -1,5 +1,81 @@
 function login() {
     const nameEl = document.querySelector("#name");
-    localStorage.setItem("userName", nameEl.value);
-    window.location.href = "play.html";
+    const passwordEl = document.querySelector("#password");
+
+    exitMessage = loadPlayerData(nameEl.value, passwordEl.value);
+
+    if (exitMessage === "Success!") {
+        window.location.href = "play.html";
+    }
+    else {
+        // Todo: Make into toast or message show later
+        return exitMessage
+    };
+}
+
+function loadPlayerData(userName_, password_) {
+    localStorage.setItem("userName", userName_);
+
+    // Load Temporary stand in Player Information
+
+    if (userName_ === "wacker") {
+        if (password_ !== "wacker") {
+            return "Error: incorrect password";
+        }
+
+        /* Hardcode Player 1:
+        User Name: wacker
+        Password: wacker
+        Pokemon:  * Ratata
+        */
+
+        const player = {
+            userName: userName_,
+            password: password_,
+            distMax: 436,
+            tlp: 5,
+            nop: 1,
+        };
+
+        playerJ = JSON.stringify(player);
+        localStorage.setItem("player", playerJ);
+    }
+    else if (userName_ === "mason") {
+        if (password_ !== "mason") {
+            return "Error: incorrect password";
+        }
+        /* Hardcode Player 2:
+        User Name: mason
+        Password: mason
+        Pokemon:  * Squirtle
+        */    
+
+        const player = {
+            userName: userName_,
+            password: password_,
+            distMax: 116,
+            tlp: 5,
+            nop: 1,
+        };
+          
+        playerJ = JSON.stringify(player);
+        localStorage.setItem("player", playerJ);
+    }
+    else if (userName_ === "Skully") {
+        const player = {
+            userName: userName_,
+            password: password_,
+            distMax: 609,
+            tlp: 15,
+            nop: 12,
+        };
+          
+        playerJ = JSON.stringify(player);
+        localStorage.setItem("player", playerJ);
+    }
+    else {
+        return "User name not recognized.";
+    }
+
+    return "Success!";
 }
---
+++ player.js
@@ -0,0 +1,37 @@
+// Load player data
+
+function loadPlayerData() {
+    const profileText = localStorage.getItem('player');
+    if (profileText) {
+        profile = JSON.parse(profileText);
+    }
+
+    // Username, Dist and Rank, Top Level Pokemon (TLP), Number of Pokemon (NoP)
+    const nameEl = document.querySelector('#name');
+    const distEl = document.querySelector('#distMax');
+    const rankEl = document.querySelector('#rank');
+    const tlpEl = document.querySelector('#tlp');
+    const nopEl = document.querySelector('#nop');
+
+    const nameTxt = profile.userName;
+    const distTxt = profile.distMax;
+    const tlpTxt = profile.tlp;
+    const nopTxt = profile.nop;
+
+    nameEl.textContent = nameTxt;
+    rankEl.textContent = calcRank(distTxt);
+    distEl.textContent = distTxt;
+    tlpEl.textContent = tlpTxt;
+    nopEl.textContent = nopTxt;
+}
+
+function calcRank(r) {
+    if (r >= 1000) return "P*";
+    else if (r >= 800) return "A";
+    else if (r >= 600) return "B";
+    else if (r >= 500) return "C";
+    else if (r > 100) return "I";
+    else return "N";    
+}
+
+loadPlayerData();
